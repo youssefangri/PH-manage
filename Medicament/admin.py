3cd449b3ee7d2506e5eca53029350e75454da36e
Youssef Angri <star.angri@gmail.com>
2018-09-27 15:21:45 +0100

    final

--- Medicament/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Medicament
-
-# Register your models here.
-
-admin.site.register(Medicament)
-
-
--- Medicament/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedicamentConfig(AppConfig):
-    name = 'Medicament'
