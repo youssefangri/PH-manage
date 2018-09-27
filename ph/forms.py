067dcc35e8799bde4695620ec99833208ad40dd1
Youssef Angri <star.angri@gmail.com>
2018-09-27 15:27:46 +0100

    first commit in profil

---
+++ ph/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from Medicament.models import Medicament
+
+class LoginFrom(forms.Form):
+	
+	username = forms.CharField(
+								
+			widget= forms.TextInput(
+										attrs={
+										"class":"form-control",
+										"id":"username",
+										"placeholder":"Username",
+												}
+									)
+								)
+			
+			
+	password = forms.CharField(
+	
+			widget = forms.PasswordInput(
+			
+										attrs={
+										"class":"form-control",
+										"id":"password",
+										"placeholder":"Password"
+											}
+										)	
+								)
+								
+User = get_user_model()
+
+class BuyForm(forms.Form):
+	Medicament = forms.CharField(widget= forms.TextInput(
+										attrs={
+										"class":"form-control",
+										"id":"Medicament",
+										"placeholder":"Medicament",
+												}
+									))
+	Quantity = forms.IntegerField(widget= forms.TextInput(
+										attrs={
+										"class":"form-control",
+										"id":"Quantity",
+										"placeholder":"Quantity",
+												}
+									))
+
+
---
+++ ph/views.py
@@ -0,0 +1,72 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LogoutView
+
+from .forms import LoginFrom, BuyForm
+from Medicament.models import Medicament
+
+User = get_user_model()
+
+class Logout(LogoutView):
+	template_name = 'home_page.html'
+	
+	
+def home_page(request):
+	queryset = Medicament.objects.all()
+	content = {	
+		"title":"Pharmacie Home page",
+	}
+	if request.user.is_authenticated():
+		content["medicament_list"]= queryset
+		content["username"]= request.user
+	return render(request, "home_page.html",content)
+
+def login_page(request):
+	form = LoginFrom(request.POST or None)
+	content = {
+		"Title":"Login Page",
+		"form":form
+	}
+	
+	if form.is_valid():
+		print(form.cleaned_data)
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			#content['form']= LoginFrom()
+			return redirect("/")
+		else :
+			content["status"]="error"
+		
+	#print(request.user.is_authenticated())
+	return render(request,"auth/login.html",content)
+
+
+def buy_page(request):
+	form = BuyForm(request.POST or None)
+	content = {
+		"Title":"Buy Page",
+		"form":form
+	}
+	
+	if form.is_valid():
+		selected_Medicament = form.cleaned_data.get("Medicament")
+		selected_Quantity = form.cleaned_data.get("Quantity")
+		try:
+			M = Medicament.objects.get(title=selected_Medicament)
+			M.quantity = (M.quantity)-(selected_Quantity)
+			if M.quantity < 0:
+				content["status"]="error"
+				pass
+			else:
+				M.save()
+				content["status"]="success"
+		except:
+			content["status"]="error"
+			pass
+	return render(request,"oper/buy.html",content)	
+	
+
+	
