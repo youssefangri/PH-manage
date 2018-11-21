1d2e2387b56fa79cf56ba848aa26bf3ff779f02b
Youssef Angri <star.angri@gmail.com>
2018-11-21 20:02:11 +0000

    add cart cart

--- ph/urls.py
+++ ph/urls.py
@@ -21,13 +21,15 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 
 from .views import home_page, login_page, buy_page, Logout
-
+from cart.views import cart_home, cart_update
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_page, name='home'),
     url(r'^login/$', login_page,name='login'),
     url(r'^buy/$', buy_page,name='buy'),
     url(r'^logout/$', Logout.as_view(), name='logout'),
+    url(r'^cart/$', cart_home, name='cart'),
+    url(r'^update/$', cart_update, name='update'),
     #url(r'^logout/$', LogoutView.as_view(), name='logout', {'next_page': '/login'}),
 ]
 
