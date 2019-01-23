375d8bb2418ad3848e4fd8bfb4d35c4fbd0f1c3f
Youssef Angri <star.angri@gmail.com>
2019-01-24 00:39:59 +0100

    add filter by date in commands history

--- commands/views.py
+++ commands/views.py
@@ -3,7 +3,7 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from .models import Command
 from cart.models import Cart
 from Medicament.models import Medicament
-
+from datetime import date
 
 def checkout(request):
     try:
@@ -41,8 +41,23 @@ def checkout(request):
     return redirect("/")
 
 def Command_history(request):
-    all_commands = Command.objects.all()
+    #all_commands = Command.objects.all()
+    today = date.today
+    #print(today().day)
+    selected_date = Command.objects.filter(timestamp__year=today().year, timestamp__month=today().month, timestamp__day=today().day)
     content = {
-        "commands" : all_commands
+        "commands" : selected_date,
+        "date" : "today"
     }
+    if request.method == 'POST':
+        s_date = request.POST.get('date')
+        qs_date = s_date.split('-')
+        if len(qs_date) == 1:
+            selected_date = Command.objects.filter(timestamp__year = qs_date[0])
+        elif len(qs_date) == 2:
+            selected_date = Command.objects.filter(timestamp__year = qs_date[1], timestamp__month = qs_date[0] )
+        elif len(qs_date) == 3:
+            selected_date = Command.objects.filter(timestamp__year = qs_date[2], timestamp__month = qs_date[1],timestamp__day=qs_date[0] )
+        content["commands"]= selected_date
+        content["date"] = s_date
     return render(request, "commands/home.html",content)
